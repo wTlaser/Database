622f1116555eef95fb8ff56862ba3e1aaf288f56
wTlaser <103103591@student.swin.edu.au>
2021-05-10 16:46:19 +1000

    Assessment

--- database.sql
+++ database.sql
@@ -1,22 +1,29 @@
+drop table if exists booking;
+drop table if exists tourevent;
+drop table if exists client;
+drop table if exists tour;
+
+
+
 Create Table Tour (
     TourName nvarchar(100),
     Description nvarchar(500)
     PRIMARY KEY (TourName)
 );
-Create Table CLient (
+Create Table Client (
     ClientId INT,
     Surname nvarchar(100) NOT NULL,
     GivenName nvarchar(100) NOT NULL,
     Gender nvarchar(1) CHECK (Gender IN ('M','F','I')),
     PRIMARY KEY (ClientId),
 );
-Create Table Event (
+Create Table TourEvent (
     TourName nvarchar(100),
     EventMonth nvarchar(3) CHECK (EventMonth IN ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')),
     EventDay INT CHECK (EventDay>=1 and EventDay<=31),
     EventYear INT CHECK (EventYear=4),
     EventFee Money NOT NULL, CHECK (EventFee>0),
-  PRIMARY KEY (TourName, EventDay, EventYear),
+  PRIMARY KEY (TourName, EventDay, EventYear, EventMonth),
   FOREIGN KEY (TourName) REFERENCES Tour(TourName),
 );
 Create Table Booking (
@@ -29,5 +36,5 @@ Create Table Booking (
     DateBooked DATE NOT NULL,
   PRIMARY KEY (ClientId, TourName, EventMonth, EventDay, EventYear),
   FOREIGN KEY (ClientId) REFERENCES Client(ClientId),
-  FOREIGN KEY (TourName, EventMonth, EventDay, EventYear) REFERENCES Event(TourName, EventMonth, EventDay, EventYear),
+  FOREIGN KEY (TourName, EventDay, EventYear, EventMonth) REFERENCES TourEvent
 );
